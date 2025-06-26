29d8960ed79855a344a9d797edeb81734d891b0f
Adam · Li <775837740@qq.com>
2025-06-26 19:57:45 +0800

    feat: implement checkout components including CheckoutFormSection, CheckoutOrderSummary, DeliveryMethodCard, and CountriesStatesAutocomplete; add payment card components for Amex, MasterCard, and Visa; enhance CheckoutPage with form handling and stock checking

---
+++ app/checkout/components/CheckoutOrderSummary.jsx
@@ -0,0 +1,75 @@
+import clsx from 'clsx'
+import { useMemo } from 'react'
+import { RiLockLine } from 'react-icons/ri'
+
+import Button from '@/components/ui/Button'
+import OrderProductCard from '@/components/OrderProductCard'
+import OrderAmountSummary from '@/components/OrderAmountSummary'
+
+import { useCartContext } from '@/app/context/CartContext'
+
+import { getFinalAmount, getSubtotal } from '@/app/utils'
+
+const CheckoutOrderSummary = ({ className }) => {
+  const { cartItems, discount, checkingStock } = useCartContext()
+
+  const subtotal = useMemo(() => getSubtotal(cartItems), [cartItems])
+  const finalAmount = useMemo(
+    () => getFinalAmount(subtotal, discount),
+    [subtotal, discount]
+  )
+
+  return (
+    <section
+      aria-describedby='checkout-form'
+      className={clsx(
+        'rounded-lg',
+        'border border-neutral-200',
+        'p-[15px] md:p-[31px]',
+        'flex flex-col gap-8',
+        className
+      )}
+    >
+      <h3 className='text-xl font-semibold'>Order Summary</h3>
+      <div
+        className={clsx('divide-y divide-neutral-300', 'flex flex-1 flex-col')}
+      >
+        <ul
+          className={clsx('divide-y divide-dashed divide-neutral-300', 'pb-8')}
+        >
+          {cartItems.map((item) => (
+            <li
+              key={item.unit.sku}
+              className={clsx('py-8 first:pt-0 last:pb-0')}
+            >
+              <OrderProductCard data={item} />
+            </li>
+          ))}
+        </ul>
+
+        <OrderAmountSummary
+          subtotal={subtotal}
+          discountCode={discount?.coupon_code}
+          discountAmount={discount?.discount_amount?.toFixed(2)}
+          discountPercentage={discount?.discount_percentage}
+          className={clsx('py-8', 'lg:flex-1', 'h-full md:h-[247px] lg:h-full')}
+        />
+
+        <div className={clsx('flex justify-between gap-4', 'pt-8')}>
+          <span className='text-2xl font-medium'>Total</span>
+          <span className='text-4xl font-semibold'>${finalAmount}</span>
+        </div>
+      </div>
+
+      <Button
+        type='submit'
+        label='Confirm order'
+        size='xl'
+        isDisabled={checkingStock}
+        startIcon={RiLockLine}
+      />
+    </section>
+  )
+}
+
+export default CheckoutOrderSummary
---
+++ app/checkout/components/DeliveryMethodCard.jsx
@@ -0,0 +1,71 @@
+import clsx from 'clsx'
+import { useCallback } from 'react'
+import { RiCheckboxCircleFill } from 'react-icons/ri'
+
+const DELIVERY_METHOD = {
+  standard: {
+    title: 'Standard',
+    days: '4-10',
+    price: null
+  },
+  express: {
+    title: 'Express',
+    days: '2-5',
+    price: '15.00'
+  }
+}
+
+const DeliveryMethodCard = ({ method, isSelected, onSelect }) => {
+  const { title, days, price } = DELIVERY_METHOD[method]
+
+  const handleKeyDown = useCallback(
+    (event) => {
+      if (event.key === 'Enter') {
+        onSelect(method)
+      }
+    },
+    [method, onSelect]
+  )
+
+  return (
+    <div
+      aria-label={title}
+      tabIndex={0}
+      role='radio'
+      aria-checked={isSelected}
+      onKeyDown={handleKeyDown}
+      onClick={() => onSelect(method)}
+      className={clsx(
+        'flex grow flex-col gap-2',
+        'min-h-[120px]',
+        'bg-white hover:bg-neutral-50',
+        'rounded-lg p-[15px]',
+        'flex-1',
+        isSelected
+          ? 'border border-indigo-600'
+          : 'border border-neutral-200 hover:border-[#e6e6e6]',
+        isSelected && 'outline-1 -outline-offset-2 outline-indigo-600',
+        'focus:outline-none',
+        'focus:ring-4 focus:ring-indigo-600/[.12] focus:ring-offset-0',
+        'cursor-pointer'
+      )}
+    >
+      <div className='flex grow gap-2 self-stretch'>
+        <div className='flex grow flex-col'>
+          <span className='font-medium'>{title}</span>
+          <span className='text-sm text-neutral-600'>{days} business days</span>
+        </div>
+
+        {isSelected && (
+          <RiCheckboxCircleFill
+            className='size-6 text-indigo-500'
+            aria-hidden
+          />
+        )}
+      </div>
+      <span className='font-medium'>{price ? `$${price}` : 'FREE'}</span>
+    </div>
+  )
+}
+
+export default DeliveryMethodCard
